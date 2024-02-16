5caad463a5d5af3e64a22534d0746c57b1774d6f
xla27 <alberto.perlini99@gmail.com>
2024-02-16 11:13:06 +0100

    removing print(os.getcwd())

--- SU2_PY/SU2/run/surrogate.py
+++ SU2_PY/SU2/run/surrogate.py
@@ -96,7 +96,6 @@ def surrogate(config):
         isPath = os.path.exists(path)
 
         with su2io.redirect_folder(path, pull, link) as push:
-            print(os.getcwd())
 
             func, dv_vec = su2io.tools.read_epm("epm.dat", func_out_name)
         
