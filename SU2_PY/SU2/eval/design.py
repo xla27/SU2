1e5a77634a271164e0f99fbfdca6ba949109508d
xla27 <alberto.perlini99@gmail.com>
2024-03-04 23:32:04 +0100

    Fixing Bug Correcting the gradient method for variables of different types

--- SU2_PY/SU2/eval/design.py
+++ SU2_PY/SU2/eval/design.py
@@ -376,8 +376,11 @@ def obj_df(dvs,config,state=None):
                 scale*=obj_dp(config, state, this_obj, def_objs)
                 sign = 1.0
 
-            if this_obj in su2io.cred_surface_map.keys():
-                grad_method = "SURROGATE"
+            if this_obj in su2io.historyOutFields:
+                if su2io.historyOutFields[this_obj]['TYPE'] == 'CREDIBILITY':
+                    grad_method = "SURROGATE"
+            else:
+                grad_method = config.get('GRADIENT_METHOD','CONTINUOUS_ADJOINT')    
 
             # Evaluate Objective Gradient
             grad = su2grad(this_obj,grad_method,config,state)
@@ -470,8 +473,11 @@ def con_dceq(dvs,config,state=None):
         global_factor = float(config['OPT_GRADIENT_FACTOR'])
         value = def_cons[this_con]['VALUE']
 
-        if this_con in su2io.cred_surface_map.keys():
-            grad_method = "SURROGATE"
+        if this_con in su2io.historyOutFields:
+            if su2io.historyOutFields[this_con]['TYPE'] == 'CREDIBILITY':
+                grad_method = "SURROGATE"
+        else:
+            grad_method = config.get('GRADIENT_METHOD','CONTINUOUS_ADJOINT')    
 
         # Evaluate Constraint Gradient
         grad = su2grad(this_con,grad_method,config,state)
@@ -575,9 +581,7 @@ def con_dcieq(dvs,config,state=None):
                 grad_method = "SURROGATE"
         else:
             grad_method = config.get('GRADIENT_METHOD','CONTINUOUS_ADJOINT')     
-
-        print(this_con, grad_method) 
-        
+       
 
         # Evaluate Constraint Gradient
         grad = su2grad(this_con,grad_method,config,state)
