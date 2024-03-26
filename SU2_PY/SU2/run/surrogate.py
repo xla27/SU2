56b1ddaf37933c23f5e8c8550fb17d06fb2eb1c2
xla27 <alberto.perlini99@gmail.com>
2024-03-26 10:58:39 +0100

    Update Switching from SMT to sklearn for surrogate modeling

--- SU2_PY/SU2/run/surrogate.py
+++ SU2_PY/SU2/run/surrogate.py
@@ -35,8 +35,9 @@ import numpy as np
 
 from .. import io as su2io
 
-from smt.surrogate_models import KRG
-from smt.utils.design_space import DesignSpace, FloatVariable
+import scipy.optimize as sci_opt
+import sklearn.gaussian_process as sklgp
+import warnings
 
 # ----------------------------------------------------------------------
 #  Surrogate Modeling
@@ -45,10 +46,6 @@ from smt.utils.design_space import DesignSpace, FloatVariable
 
 def surrogate(config):
     """info = SU2.run.surrogate(config)
-
-    Runs the interface with Surrogate Modeling Toolbox
-    Copyright (c) 2017, SMT developers
-    All rights reserved.
 
     Assumptions:
         Does not rename restart filename to solution filename
@@ -75,13 +72,14 @@ def surrogate(config):
 
     # verbose
     if konfig.get("CONSOLE", "VERBOSE") in ["QUIET", "CONCISE"]:
-        print_global = True
-    else:
-        print_global = False
+        warnings.filterwarnings('ignore') 
 
     # design space definition
     def_dv = konfig.DEFINITION_DV
     n_dv = sum(def_dv["SIZE"])
+    dv_scales = def_dv["SCALE"]
+    dv_size   = def_dv["SIZE"]
+    global_factor = float(konfig["OPT_GRADIENT_FACTOR"])
     bound_upper = float(konfig.OPT_BOUND_UPPER)   
     bound_lower = float(konfig.OPT_BOUND_LOWER)  
     relax_factor = float(config.OPT_RELAX_FACTOR)
@@ -91,14 +89,16 @@ def surrogate(config):
     # reading the data from past EPM
     pull = []
     link = []
-    i = 1
+
 
     xt = np.empty((0, n_dv))
     yt = np.array([])
-    
+
+    # looping on INIT folders to read EPM data
+    i_init = 1
     isPath = True
     while isPath:
-        path = "../../DSN_" + str(i).zfill(3) + "/EPM"
+        path = "../../../INITIALIZATION/INIT_" + str(i_init).zfill(3) + "/EPM"
         isPath = os.path.exists(path)
 
         if isPath:
@@ -111,43 +111,96 @@ def surrogate(config):
             xt = np.vstack((xt, np.array(dv_vec)))
             yt = np.append(yt, func)
 
-        i += 1
+        i_init += 1
+
     
-    """increasing the characteristic lengthscales, if the training points are less than n_dv
-    the GP is kept isotropic, if they are greater or equal than n_dv the process becomes
-    anisotropic
-    """
+    # looping on DSN folders to read EPM data
+    i_dsn = 1    
+    isPath = True
+    while isPath:
+        path = "../../DSN_" + str(i_dsn).zfill(3) + "/EPM"
+        isPath = os.path.exists(path)
+
+        if isPath:
+
+            with su2io.redirect_folder(path, pull, link) as push:
+
+                func, dv_vec = su2io.tools.read_epm("epm.dat", objective)
+                
+            
+            xt = np.vstack((xt, np.array(dv_vec)))
+            yt = np.append(yt, func)
+
+        i_dsn += 1
+    
+ 
     n_data = yt.shape[0]
 
-    theta0 = [1e-2]
-    if n_data >= n_dv:
-        theta0 = [1e-2] * n_dv 
+    theta0 = np.array([1e-1]).reshape(1,1)
+    # if n_data >= n_dv:
+    #     theta0 = np.array([1e-1] * n_dv).reshape(n_dv,1) 
     
-    # design space
-    des_sp = []
-    for i in range(0, n_dv):
-        des_sp.append(FloatVariable(xb_low[i], xb_up[i]))
-    design_space = DesignSpace(des_sp)
-
-
     # GP instantiation
-    sm = KRG(design_space=design_space, theta0=theta0, print_global=print_global)
-    sm.set_training_values(xt, yt)
+    kernel = sklgp.kernels.ConstantKernel(1.0, (1e-1**3, 1e1**2)) * sklgp.kernels.RBF(length_scale=theta0, length_scale_bounds=(1e-2, 1e2))
+    gp = sklgp.GaussianProcessRegressor(kernel=kernel, optimizer='fmin_l_bfgs_b', n_restarts_optimizer=50, alpha=1e-7)
+
 
     if n_data == 1:
+
         raw_gradient = [0.0] * n_dv
 
-    else:
-        sm.train()
+        # rand = np.random.rand(n_dv)
+        # raw_gradient = rand / np.linalg.norm(rand, 2)
+
+        # # making the random first guess have a 1E-6 magnitude
+        # raw_gradient = 1E-6 * raw_gradient
+        # k = 0
+        # for i_dv,dv_scl in enumerate(dv_scales):
+        #     for i_grd in range(dv_size[i_dv]):
+        #         raw_gradient[k] = raw_gradient[k] * dv_scl / global_factor 
+        #         k = k + 1
+
+    elif n_data > 1 and n_data < n_dv:
+        gp.fit(xt, yt)
 
         # derivatives computation
-        derivatives = np.empty((n_data, 0))
-        for i in range(0, n_dv):
-            derivatives = np.hstack((derivatives, sm.predict_derivatives(xt, i)))
+        def fun_prediction(x, gp):
+            fun_pred = gp.predict(x.reshape(1, -1))
+            return fun_pred
 
         # querying the derivative at the last xt
-        raw_gradient = derivatives[-1,:]
+        raw_gradient = sci_opt.approx_fprime(xt[-1,:], fun_prediction, [np.sqrt(np.finfo(float).eps)]*n_dv, gp)
+
+        # finding the reduction factor necessary for having a raw_gradient with a norm smaller that 5e-6 after the scalings 
+        scaled_grad = raw_gradient
+        k = 0
+        for i_dv,dv_scl in enumerate(dv_scales):
+            for i_grd in range(dv_size[i_dv]):
+                scaled_grad[k] = scaled_grad[k] / dv_scl 
+                k = k + 1
+        
+        i = 0
+        reduction_factor = 0.5**i
+        while np.linalg.norm(scaled_grad, 2) > 5e-6 / global_factor:
+            i += 1
+            reduction_factor = 0.5**i
+            scaled_grad = scaled_grad * 0.5
+        
+        raw_gradient = raw_gradient * reduction_factor
+
+
+    else:
+        gp.fit(xt, yt)
+
+        # derivatives computation
+        def fun_prediction(x, gp):
+            fun_pred = gp.predict(x.reshape(1, -1))
+            return fun_pred
+
+        # querying the derivative at the last xt
+        raw_gradient = sci_opt.approx_fprime(xt[-1,:], fun_prediction, [np.sqrt(np.finfo(float).eps)]*n_dv, gp)
     
+
 
     # info out
     info = su2io.State()
