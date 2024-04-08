458c0fe0e62e6ef80dfdce1337e16f5c80753941
xla27 <alberto.perlini99@gmail.com>
2024-04-08 16:13:30 +0200

    Update Imposing a check inside aerodynamics() that when OPT_OBJECTIVE is CUSTOM_OBJFUNC, then the entry in state['FUNCTIONS']['COMBO'] is substituted with state['FUNCTIONS']['CUSTOM_OBJFUNC']

--- SU2_PY/SU2/eval/functions.py
+++ SU2_PY/SU2/eval/functions.py
@@ -322,6 +322,13 @@ def aerodynamics( config, state=None ):
 
     #: with output redirection
     su2io.update_persurface(konfig,state)
+    # check for custom objectives
+    for key in state['FUNCTIONS']:
+        if config['OPT_OBJECTIVE'] == 'CUSTOM_OBJFUNC':
+            custom = su2util.ordered_bunch()
+            custom['CUSTOM_OBJFUNC'] = state['FUNCTIONS']['COMBO']
+            del state['FUNCTIONS']['COMBO']
+            state['FUNCTIONS']['CUSTOM_OBJFUNC'] = custom['CUSTOM_OBJFUNC']
     # return output
     funcs = su2util.ordered_bunch()
     for key in state['FUNCTIONS']:
