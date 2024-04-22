b1d05df1f0f3e5a42a10aaa755eef52811cef8a5
xla27 <alberto.perlini99@gmail.com>
2024-04-22 18:11:45 +0200

    Update Surrogate Gradient Scaling

--- SU2_PY/SU2/run/surrogate.py
+++ SU2_PY/SU2/run/surrogate.py
@@ -160,7 +160,7 @@ def surrogate(config):
         #         raw_gradient[k] = raw_gradient[k] * dv_scl / global_factor 
         #         k = k + 1
 
-    elif n_data > 1 and n_data < n_dv:
+    else: #n_data > 1 and n_data < n_dv:
         gp.fit(xt, yt)
 
         # derivatives computation
@@ -189,16 +189,16 @@ def surrogate(config):
         raw_gradient = raw_gradient * reduction_factor
 
 
-    else:
-        gp.fit(xt, yt)
+    # else:
+    #     gp.fit(xt, yt)
 
-        # derivatives computation
-        def fun_prediction(x, gp):
-            fun_pred = gp.predict(x.reshape(1, -1))
-            return fun_pred
+    #     # derivatives computation
+    #     def fun_prediction(x, gp):
+    #         fun_pred = gp.predict(x.reshape(1, -1))
+    #         return fun_pred
 
-        # querying the derivative at the last xt
-        raw_gradient = sci_opt.approx_fprime(xt[-1,:], fun_prediction, [np.sqrt(np.finfo(float).eps)]*n_dv, gp)
+    #     # querying the derivative at the last xt
+    #     raw_gradient = sci_opt.approx_fprime(xt[-1,:], fun_prediction, [np.sqrt(np.finfo(float).eps)]*n_dv, gp)
     
 
 
