10e2c1652797865f6978e259b792e97abcba3928
xla27 <alberto.perlini99@gmail.com>
2025-07-10 17:32:51 +0200

    Improved handling for ADAP errors

--- Common/src/CConfig.cpp
+++ Common/src/CConfig.cpp
@@ -5708,18 +5708,14 @@ void CConfig::SetPostprocessing(SU2_COMPONENT val_software, unsigned short val_i
           if (!DiscreteAdjoint)
             SU2_MPI::Error("Adaptation sensor GOAL can only be computed for MATH_PROBLEM = DISCRETE_ADJOINT.", CURRENT_FUNCTION);
         }
-        if (nemo) {
-          if (Adap_Sensor[iSensor] == "GOAL")
-            SU2_MPI::Error("Adaptation sensor GOAL is not available for SOLVER = NEMO_EULER and SOLVER = NEMO_NAVIER_STOKES.", CURRENT_FUNCTION);
-
-          if (Adap_Sensor[iSensor] == "TEMPERATURE")
-            SU2_MPI::Error("Adaptation sensor TEMPERATURE is not available for SOLVER = NEMO_EULER and SOLVER = NEMO_NAVIER_STOKES.", CURRENT_FUNCTION);
-
-          if (Adap_Sensor[iSensor] == "ENERGY")
-            SU2_MPI::Error("Adaptation sensor ENERGY is not available for SOLVER = NEMO_EULER and SOLVER = NEMO_NAVIER_STOKES.", CURRENT_FUNCTION);
-
-          if (Adap_Sensor[iSensor] == "DENSITY")
-            SU2_MPI::Error("Adaptation sensor DENSITY is not available for SOLVER = NEMO_EULER and SOLVER = NEMO_NAVIER_STOKES.", CURRENT_FUNCTION);
+        if (Kind_Solver == MAIN_SOLVER::NEMO_EULER || Kind_Solver == MAIN_SOLVER::NEMO_NAVIER_STOKES) {
+          if (Adap_Sensor[iSensor] == "GOAL" || Adap_Sensor[iSensor] == "TEMPERATURE" ||
+              Adap_Sensor[iSensor] == "ENERGY" || Adap_Sensor[iSensor] == "DENSITY")
+            SU2_MPI::Error(string("Adaptation sensor ") + Adap_Sensor[iSensor] + string(" not available for NEMO problems."), CURRENT_FUNCTION);
+        }
+        if (Kind_Solver == MAIN_SOLVER::INC_EULER || Kind_Solver == MAIN_SOLVER::INC_NAVIER_STOKES || Kind_Solver == MAIN_SOLVER::INC_RANS) {
+          if (Adap_Sensor[iSensor] == "GOAL" || Adap_Sensor[iSensor] == "ENERGY")
+            SU2_MPI::Error(string("Adaptation sensor ") + Adap_Sensor[iSensor] + string(" not available for INC problems."), CURRENT_FUNCTION);
         }
       }
       else {
